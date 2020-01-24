b8f017c0dd024cc33508c96f255428bafbb6e449
Murray Cumming <murrayc@murrayc.com>
2020-01-24 23:48:06 +0100

    Use SDL2

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,34 @@
 #include <iostream>
 #include <cstdlib>
 
+#include <SDL2/SDL.h>
+// #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
+#include <unistd.h>
+
 int main() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    std::cerr << "SDL_Init() failed: " << SDL_GetError();
+    return EXIT_FAILURE;
+  }
+
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+  auto win = SDL_CreateWindow("Exam ple",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              1000, 1000, 0);
+  auto gl_context = SDL_GL_CreateContext(win);
+
+  sleep(2);
+
+
+  SDL_GL_DeleteContext(gl_context);
+  SDL_DestroyWindow(win);
+  SDL_Quit();
   return EXIT_SUCCESS;
 }
