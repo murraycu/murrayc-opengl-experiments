7bda43557139bd4dd48fc5bbb0442d747dbf8f3e
Murray Cumming <murrayc@murrayc.com>
2020-01-25 00:07:26 +0100

    Use glew

--- src/main.cc
+++ src/main.cc
@@ -2,27 +2,38 @@
 #include <cstdlib>
 
 #include <SDL2/SDL.h>
-// #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
+#include <GL/glew.h>
 
 #include <unistd.h>
 
 int main() {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    std::cerr << "SDL_Init() failed: " << SDL_GetError();
+    std::cerr << "SDL_Init() failed: " << SDL_GetError() << std::endl;;
     return EXIT_FAILURE;
   }
 
-  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 
   auto win = SDL_CreateWindow("Exam ple",
                               SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED,
-                              1000, 1000, 0);
+                              1000, 1000,
+                              SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
   auto gl_context = SDL_GL_CreateContext(win);
+
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,32);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,16);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
+
+  // glewExperimental = GL_TRUE;
+  auto const res = glewInit();
+  if(res != GLEW_OK) {
+    std::cerr << "glewInit() failed: " << glewGetErrorString(res) << std::endl;;
+    return EXIT_FAILURE;
+  }
 
   sleep(2);
 
