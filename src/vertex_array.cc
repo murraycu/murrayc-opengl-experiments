1ad1a755d515178ef427df51250560d889a84da5
Murray Cumming <murrayc@murrayc.com>
2020-01-28 21:48:12 +0100

    VertexArray: Use sizeof(type) rather than sizeof(container[0])
    That seems a little more pleasant.

--- src/vertex_array.cc
+++ src/vertex_array.cc
@@ -21,16 +21,16 @@ void VertexArray::bind_buffer() {
   glBindVertexArray(id_);
 
   glBindBuffer(GL_ARRAY_BUFFER, buffer_.id());
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_[0]) * vertices_.size(), &vertices_[0], GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices_.size(), &vertices_[0], GL_STATIC_DRAW);
 
   // Describe the arrangement of bytes in the Vertex:
   // These same attribute ids are used later in the call to
   // glBindAttribLocation().
   glEnableVertexAttribArray(AttributeID::POSITION);
-  glVertexAttribPointer(AttributeID::POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(vertices_[0]), reinterpret_cast<void*>(offsetof(Vertex, position_)));
+  glVertexAttribPointer(AttributeID::POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, position_)));
 
   glEnableVertexAttribArray(AttributeID::TEXTURE_POS);
-  glVertexAttribPointer(AttributeID::TEXTURE_POS, 2, GL_FLOAT, GL_FALSE, sizeof(vertices_[0]), reinterpret_cast<void*>(offsetof(Vertex, texture_pos_)));
+  glVertexAttribPointer(AttributeID::TEXTURE_POS, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, texture_pos_)));
 
   glBindVertexArray(0);
 }
